9601efbce178e3970582ab4fd6c722cb08ccda5d
Burhanuddin saifee <burhansaifee2003@gmail.com>
2025-02-12 00:15:52 +0530

    Burhan-Portfolio 1 commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FaGithub, FaInstagram } from 'react-icons/fa';
+import { AiFillLinkedin } from 'react-icons/ai';
+import { FiMail } from 'react-icons/fi';
+
+
+const Footer = () => {
+	const year = new Date().getFullYear();
+
+	return (
+		<div className='bg-slate-900 p-4' style={{ cursor: 'default' }}>
+			<div className='flex justify-between flex-wrap gap-4'>
+				<p className='text-white text-center w-full sm:w-auto font-light'>© {year} Burhanuddin. All rights reserved.</p>
+				<div className='text-white flex justify-around sm:w-[250px] w-full'>
+
+					<a href="burhansaifee2003@gmail.com" className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
+						<FiMail className='text-xl' />
+					</a>
+					<a href="https://www.linkedin.com/in/burhanuddin-saifee-6aa15b255/" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
+						<AiFillLinkedin className='text-xl' />
+					</a>
+					<a href="https://github.com/burhansaifee?tab=overview&from=2025-02-01&to=2025-02-11" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }} >
+						<FaGithub className='text-xl' />
+					</a>
+					<a href="https://www.instagram.com/saifee.b0?igsh=c3c0bDN0anVlcXZo" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
+						<FaInstagram className='text-xl' />
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Footer;
+
